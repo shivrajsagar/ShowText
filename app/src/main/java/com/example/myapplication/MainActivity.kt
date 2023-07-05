91bb1160c248d5e46227ab858099d9b96e84b2f0
shivrajsagar <shivsagar.sagar294@gmail.com>
2023-07-05 16:02:52 +0530

    Commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val ll = LinearLayout(this)
+//        ll.orientation = LinearLayout.VERTICAL
+//        ll.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+//            LinearLayout.LayoutParams.MATCH_PARENT)
+//        ll.gravity = LinearLayout.TEXT_ALIGNMENT_CENTER
+//
+//
+//        // add TextView
+//        val showTextInput = TextView(this)
+//        showTextInput.text = "Hello Guys"
+//
+//        val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+//            LinearLayout.LayoutParams.WRAP_CONTENT)
+//        showTextInput.layoutParams = layoutParams
+//        layoutParams.setMargins(20, 20, 20, 20)
+//        layoutParams.gravity = LinearLayout.VERTICAL
+//        showTextInput.textSize = 16f
+//        showTextInput.setPadding(5, 3, 0, 3)
+//        ll.addView(showTextInput)
+
+
+        val layout = findViewById<RelativeLayout>(R.id.root)
+
+        // Create TextView programmatically.
+        val textView = TextView(this)
+
+        // setting height and width
+        textView.layoutParams= RelativeLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT)
+        // setting text
+        textView.setText("GEEKSFORGEEKS")
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40f)
+        textView.setTextColor(Color.MAGENTA)
+        // onClick the text a message will be displayed "HELLO GEEK"
+        textView.setOnClickListener()
+        {
+            Toast.makeText(this@MainActivity, "HELLO GEEK",
+                Toast.LENGTH_LONG).show()
+        }
+
+        // Add TextView to LinearLayout
+        layout ?.addView(textView)
     }
 }
--- mylibrary/src/main/java/com/example/mylibrary/Demo.kt
+++ mylibrary/src/main/java/com/example/mylibrary/Demo.kt
@@ -2,7 +2,11 @@ package com.example.mylibrary
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Color
+import android.util.TypedValue
+import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,25 +17,28 @@ class Demo : Activity() {
 
         fun  showText(context: Context?, Message: String?) {
             // create LinearLayout
-            val ll = LinearLayout(context!!.applicationContext)
-            ll.orientation = LinearLayout.VERTICAL
-            ll.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT)
-            ll.gravity = LinearLayout.TEXT_ALIGNMENT_CENTER
+//            val layout = findViewById<RelativeLayout>(R.id.root)
 
+            // Create TextView programmatically.
+            val textView = TextView(context)
 
-            // add TextView
-            val showTextInput = TextView(context!!.applicationContext)
-            showTextInput.text = Message
+            // setting height and width
+            textView.layoutParams= RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+            // setting text
+            textView.setText("GEEKSFORGEEKS")
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40f)
+            textView.setTextColor(Color.MAGENTA)
+            // onClick the text a message will be displayed "HELLO GEEK"
+//            textView.setOnClickListener()
+//            {
+//                Toast.makeText(context, "HELLO GEEK",
+//                    Toast.LENGTH_LONG).show()
+//            }
 
-            val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT)
-            showTextInput.layoutParams = layoutParams
-            layoutParams.setMargins(20, 20, 20, 20)
-            layoutParams.gravity = LinearLayout.VERTICAL
-            showTextInput.textSize = 16f
-            showTextInput.setPadding(5, 3, 0, 3)
-            ll.addView(showTextInput)
+            // Add TextView to LinearLayout
+//            layout ?.addView(textView)
         }
     }
 }
