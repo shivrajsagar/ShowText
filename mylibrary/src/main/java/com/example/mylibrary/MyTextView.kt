4016a8827417e3d2a125d65d13bd574c9e9e95b9
shivrajsagar <shivsagar.sagar294@gmail.com>
2023-07-06 15:00:06 +0530

    Commit

--- mylibrary/src/main/java/com/example/mylibrary/MyTextView.kt
+++ mylibrary/src/main/java/com/example/mylibrary/MyTextView.kt
@@ -10,7 +10,7 @@ import com.example.mylibrary.MyTextView.Companion.DEFAULT_TEXT_SIZE_SP
 class MyTextView : AppCompatTextView {
     companion object {
         private const val DEFAULT_TEXT_SIZE_SP = 20f
-        private const val DEFAULT_TEXT_COLOR = android.R.color.holo_red_dark
+        private const val DEFAULT_TEXT_COLOR = android.R.color.holo_green_dark
     }
     constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(context!!,
         attrs,
