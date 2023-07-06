583db0e3b408cb0debc3e3dea098d03c68e4d3be
shivrajsagar <shivsagar.sagar294@gmail.com>
2023-07-06 11:47:43 +0530

    Commit

--- mylibrary/src/main/java/com/example/mylibrary/CustomTextView.kt
+++ mylibrary/src/main/java/com/example/mylibrary/CustomTextView.kt
@@ -4,10 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
 
-class CustomTextView @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : AppCompatTextView(context, attrs, defStyleAttr) {
-    
+class CustomTextView(context: Context) : AppCompatTextView(context) {
+
 }
---
+++ mylibrary/src/main/java/com/example/mylibrary/PinView.kt
@@ -0,0 +1,9 @@
+package com.example.mylibrary
+
+import android.content.Context
+import androidx.appcompat.widget.AppCompatEditText
+
+class PinView(context: Context) : AppCompatEditText(context) {
+
+
+}
