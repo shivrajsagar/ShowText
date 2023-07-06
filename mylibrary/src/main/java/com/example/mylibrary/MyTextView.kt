a1a54887548f73bc9ee108fb7dcd8fffb432912e
shivrajsagar <shivsagar.sagar294@gmail.com>
2023-07-06 14:59:29 +0530

    Commit

--- mylibrary/src/main/java/com/example/mylibrary/MyTextView.kt
+++ mylibrary/src/main/java/com/example/mylibrary/MyTextView.kt
@@ -4,19 +4,21 @@ import android.content.Context
 import android.os.Build
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
+import com.example.mylibrary.MyTextView.Companion.DEFAULT_TEXT_SIZE_SP
 
 
 class MyTextView : AppCompatTextView {
+    companion object {
+        private const val DEFAULT_TEXT_SIZE_SP = 20f
+        private const val DEFAULT_TEXT_COLOR = android.R.color.holo_red_dark
+    }
     constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(context!!,
         attrs,
         defStyle) {
         init()
     }
 
-    companion object {
-        private const val DEFAULT_TEXT_SIZE_SP = 14f
-        private const val DEFAULT_TEXT_COLOR = android.R.color.holo_red_dark
-    }
+
 
     constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs) {
         init()
@@ -27,12 +29,12 @@ class MyTextView : AppCompatTextView {
     }
 
     private fun init() {
-        text = "blablabla"
-        setTextSize(CustomTextView.DEFAULT_TEXT_SIZE_SP)
+        text = "This is a textView from Package"
+        textSize = MyTextView.DEFAULT_TEXT_SIZE_SP
 
         // Set default text color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            setTextColor(context.getColor(CustomTextView.DEFAULT_TEXT_COLOR))
+            setTextColor(context.getColor(MyTextView.DEFAULT_TEXT_COLOR))
         }
     }
 }
